71c0dbf479ac1928038e45d3296707af3fac586b
Laura Belén Yachelini <lbyachelini@gmail.com>
2024-04-14 06:49:20 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Laura Belen Yachelini on 14/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
